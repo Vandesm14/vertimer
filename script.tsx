358c9f5b30eec7a74c33fec8f1bec099bedce5ef
Vandesm14 <vandesm14@gmail.com>
2022-06-16 15:50:12 -1000

    add remove functoin

--- script.tsx
+++ script.tsx
@@ -2,13 +2,25 @@ import { useReducer, useState } from "preact/hooks";
 import { render } from "preact";
 
 interface Slot {
+  id: number;
   time: number;
   relative: boolean;
 }
 
-const newSlot: () => Slot = () => ({ time: 0, relative: true });
+const gid = () => Math.round(Math.random() * 10 ** 8);
+
+const newSlot: () => Slot = () => ({
+  id: gid(),
+  time: 0,
+  relative: true,
+});
 
 type Transaction = (slots: Slot[]) => Slot[];
+
+const removeAtIndex = (arr: Array<any>, index: number) => [
+  ...arr.slice(0, index),
+  ...arr.slice(index + 1),
+];
 
 function App() {
   const [slots, alterSlots] = useReducer<Slot[], Transaction>(
@@ -17,11 +29,25 @@ function App() {
   );
 
   const addSlot = () => alterSlots((list) => [...list, newSlot()]);
+  const removeSlot = (id: Slot["id"]) =>
+    alterSlots((list) =>
+      removeAtIndex(
+        slots,
+        list.findIndex((el) => el.id === id)
+      )
+    );
 
   return (
     <main>
       <button onClick={addSlot}>Add</button>
-      Hello!
+      <ul>
+        {slots.map((slot) => (
+          <li>
+            {slot.id}{" "}
+            <button onClick={() => removeSlot(slot.id)}>Remove</button>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
