47b51206174156e924b2ac521b6ee63a851fd581
Vandesm14 <vandesm14@gmail.com>
2022-06-16 15:40:05 -1000

    add "addSlot" reducer

--- script.tsx
+++ script.tsx
@@ -1,8 +1,29 @@
-import { useState } from 'preact/hooks';
-import { render } from 'preact';
+import { useReducer, useState } from "preact/hooks";
+import { render } from "preact";
+
+interface Slot {
+  time: number;
+  relative: boolean;
+}
+
+const newSlot: () => Slot = () => ({ time: 0, relative: true });
+
+type Transaction = (slots: Slot[]) => Slot[];
 
 function App() {
-  return <h1>Hello World!</h1>;
+  const [slots, alterSlots] = useReducer<Slot[], Transaction>(
+    (slots, transaction) => transaction(slots),
+    []
+  );
+
+  const addSlot = () => alterSlots((list) => [...list, newSlot()]);
+
+  return (
+    <main>
+      <button onClick={addSlot}>Add</button>
+      Hello!
+    </main>
+  );
 }
 
 render(<App />, document.body);
